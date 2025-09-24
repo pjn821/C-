5d403665e79f7d859d49155e57ac7ce1742875b7
pjn <paulomarcal771@gmail.com>
2025-09-24 15:09:16 +0100

    segundo codigo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome.");
+
+string user;
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+user = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+Console.WriteLine("Olá " + user + " , seja bem vindo.");
