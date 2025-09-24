9fe757db50dbf8ce04dcb9ecec2cde4b784b215c
pjn <paulomarcal771@gmail.com>
2025-09-24 18:30:31 +0100

    new file: variavel

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System.Net;
 
+
+
 Console.WriteLine("Digite seu nome.");
 string user;
 user = Console.ReadLine();
 
 Console.WriteLine("qual a sua idade");
-string years;
-years = Console.ReadLine();
+int years;
+years = Convert.ToInt32(Console.ReadLine());
 
 
 Console.WriteLine("Olá " + user + " , tens " + years + " anos.");
---
+++ doido2/variavel.cs
@@ -0,0 +1,31 @@
+﻿// dados pessoais 
+        string name;
+        int idade;
+        char sexo;
+        string profissao;
+        double salario;
+
+        // input
+        Console.WriteLine("Digite os seus dados.");
+
+        Console.WriteLine("Name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Idade: ");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sexo: ");
+        sexo = Console.ReadLine()[0];
+
+        Console.WriteLine("Profissão: ");
+        profissao = Console.ReadLine();
+
+        Console.WriteLine("Salário: ");
+        salario = Convert.ToDouble(Console.ReadLine());
+
+        // saída/display
+        Console.WriteLine("Olá, " + name + " . Segue abaixo os teus dados");
+        Console.WriteLine("Idade: " + idade + " anos");
+        Console.WriteLine("Sexo: " + sexo);
+        Console.WriteLine("Profissão: " + profissao);
+        Console.WriteLine("Salário: " + salario + "€");
