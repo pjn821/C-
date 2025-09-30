7ead30325a5b335faac0c2db6050b502fef4e4ad
pjn <paulomarcal771@gmail.com>
2025-09-30 17:17:32 +0100

    operadores compostos

---
+++ operadores compostos/Program.cs
@@ -0,0 +1,16 @@
+﻿int calorias;
+
+// quntas calorias
+Console.Write("quantidade de calorias consumidas hoje: ");
+calorias = Convert.ToInt32(Console.ReadLine());
+
+// verificar calorias 
+
+if (calorias > 1200 && calorias < 1900)
+{
+    Console.WriteLine("Dentro da faixa");
+}
+else
+{
+    Console.WriteLine("fora da faixa");
+}
