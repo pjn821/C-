89383e7d3148c5294e5884c7008ca25e91bd2aef
pjn <paulomarcal771@gmail.com>
2025-09-30 17:57:56 +0100

    operadorescompostos matematica

---
+++ operadorescompostos2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿double valor;
+
+// qual o valor
+Console.Write("qual o valor do produto: ");
+valor = Convert.ToDouble(Console.ReadLine());
+
+// verificaçao do valor
+valor -= (valor * 0.12);
+//valor = valor - (valor * 0.12);
+
+// impressao do valor
+Console.WriteLine("O produto custa " + valor + "€");
