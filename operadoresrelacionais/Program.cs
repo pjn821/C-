370e9c3b419b1caab5315e25915bb30c63e0ef52
pjn <paulomarcal771@gmail.com>
2025-09-30 16:47:39 +0100

    operadore relacionais

---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,25 @@
+﻿int idade;
+
+// pedir idade
+
+Console.Write("Digite sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+// verificaçao da idade
+
+if (idade >= 18)
+{
+    Console.WriteLine("Maior de Idade");
+}
+else
+{
+    Console.WriteLine("Menor de idade");
+}
+
+// par ou impar 
+if ((idade % 2) == 0)
+{
+    Console.WriteLine("numero par");
+} else {
+    Console.WriteLine("numero impar");
+}
