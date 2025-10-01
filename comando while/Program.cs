59c055a44d2ad9d7535d92837dccaf9c79fa6c91
pjn <paulomarcal771@gmail.com>
2025-10-01 13:58:01 +0100

    comando while

---
+++ comando while/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+
+Console.Write("digite numero inteiro positivo");
+numero = Convert.ToInt32(Console.ReadLine());
+while (numero < 0)
+{
+    Console.WriteLine("o numero digitado é negativo");
+
+    Console.Write("digite numero inteiro positivo");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+
+// display
+
+Console.WriteLine("o numero: " + numero);
