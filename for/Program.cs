43657e2ada5b0488317935c14410e60a35e42996
pjn <paulomarcal771@gmail.com>
2025-10-04 21:46:23 +0100

    for

---
+++ for/Program.cs
@@ -0,0 +1,21 @@
+﻿int n;
+double media = 0;
+
+// pedir nota
+
+Console.Write("Digite a quantidade de notas: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Digite a nota " + (i + 1) + ": ");
+    double nota = Convert.ToDouble(Console.ReadLine());
+
+    media = media + nota;
+
+}
+media = media/n;
+
+// display
+
+Console.WriteLine("\nMedia: " + media);
